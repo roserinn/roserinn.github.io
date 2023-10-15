858fca93f45e82939206f3f5f6d46c087b99a757
Rina <lovelypeach224@gmail.com>
2023-10-15 19:56:51 +0300

    advantages section animation added

--- script.js
+++ script.js
@@ -1,34 +1,17 @@
+// ------------------- burger -----------------------
 
+const burgerIcon = document.querySelector('.burger-icon');
+const sideMenu = document.querySelector('.side-menu');
+console.log(burgerIcon)
 
-// import { CustomEase } from "gsap/CustomEase";
-// import { RoughEase, ExpoScaleEase, SlowMo } from "gsap/EasePack";
-
-// import { Flip } from "gsap/Flip";
-// import { ScrollTrigger } from "gsap/ScrollTrigger";
-// import { Observer } from "gsap/Observer";
-// import { ScrollToPlugin } from "gsap/ScrollToPlugin";
-// import { Draggable } from "gsap/Draggable";
-// import { MotionPathPlugin } from "gsap/MotionPathPlugin";
-// import { EaselPlugin } from "gsap/EaselPlugin";
-// import { PixiPlugin } from "gsap/PixiPlugin";
-// import { TextPlugin } from "gsap/TextPlugin";
-
-gsap.registerPlugin(ScrollTrigger);
-
-gsap.from('.advantages__card', {
-  duration: 1.5,
-  x: -100,
-  opacity: 0,
-  // scrollTrigger: {
-  //   trigger: '.advantages__card',
-  //   start: 400
-  // },
-  stagger: {
-    each: 0.5,
-    from: "start",
-  }
+burgerIcon.addEventListener('click', () => {
+  sideMenu.classList.toggle('open');
+  burgerIcon.classList.add('burger-menu__open');
 });
 
+// --------------------------- sections Anim ------------------------------
+                            
+// first section
 
 gsap.from('.anim', {
   duration: 1, y: -100, ease: "expoScale(0.5,7,none)", opacity: 0
@@ -40,51 +23,41 @@ gsap.from('.info__block', {
 
 
 
+// advantages section
 
+gsap.registerPlugin(ScrollTrigger);
 
-// window.onscroll = () => {
-//   advantagesCardAnim(advantagesCards)
-// }
+gsap.from('.advantages__card', {
+  duration: 1.5,
+  x: -100,
+  opacity: 0,
 
-// const advantagesCards = document.querySelectorAll('.advantages__card');
+  stagger: {
+    each: 0.5,
+    from: "start",
+  }, 
 
-// function advantagesCardAnim(elements) {
-//   const windowHeight = window.innerHeight;
-// elements.forEach(card => {
-//   const top = window.scrollY;
-//   const offset = card.offsetTop;
-//   const offsetHeight = card.offsetHeight;
-
-// if (top + windowHeight > offset && top < offset + offsetHeight ) {
-//   gsap.from('.advantages__card', {
-//     duration: 1.5,
-//       opacity: 0,
-//       x: -100,   
-//       stagger: {
-//         each: 0.5, 
-//         from: "start",
-//       }
-//     })
-  
-  
-// }
-
-
-
-// });
-
-// }
-
-
-
-
-const burgerIcon = document.querySelector('.burger-icon');
-const sideMenu = document.querySelector('.side-menu');
-console.log(burgerIcon)
-
-burgerIcon.addEventListener('click', () => {
-  sideMenu.classList.toggle('open');
-  burgerIcon.classList.add('burger-menu__open');
+  scrollTrigger: {
+    markers: true,
+    trigger: ".advantages",
+    start: "top center", 
+    end: "bottom center", 
+  },
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
