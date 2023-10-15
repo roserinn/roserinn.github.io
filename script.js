63f32a2cd0e493be727a27d86594a418199cb477
Rina <lovelypeach224@gmail.com>
2023-10-15 16:09:53 +0300

    добавлен адаптив

--- script.js
+++ script.js
@@ -13,12 +13,69 @@
 // import { PixiPlugin } from "gsap/PixiPlugin";
 // import { TextPlugin } from "gsap/TextPlugin";
 
+gsap.registerPlugin(ScrollTrigger);
 
-// gsap.registerPlugin(Flip,ScrollTrigger,Observer,ScrollToPlugin,Draggable,MotionPathPlugin,EaselPlugin,PixiPlugin,TextPlugin,RoughEase,ExpoScaleEase,SlowMo,CustomEase);
+gsap.from('.advantages__card', {
+  duration: 1.5,
+  x: -100,
+  opacity: 0,
+  // scrollTrigger: {
+  //   trigger: '.advantages__card',
+  //   start: 400
+  // },
+  stagger: {
+    each: 0.5,
+    from: "start",
+  }
+});
+
+
+gsap.from('.anim', {
+  duration: 1, y: -100, ease: "expoScale(0.5,7,none)", opacity: 0
+})
 
 gsap.from('.info__block', {
-  duration: 1, x: -300, ease: "expo.in"
+  duration: 2, x: -500, ease: "expoScale(0.5,7,none)", opacity: 0, delay: 1
 });
+
+
+
+
+
+// window.onscroll = () => {
+//   advantagesCardAnim(advantagesCards)
+// }
+
+// const advantagesCards = document.querySelectorAll('.advantages__card');
+
+// function advantagesCardAnim(elements) {
+//   const windowHeight = window.innerHeight;
+// elements.forEach(card => {
+//   const top = window.scrollY;
+//   const offset = card.offsetTop;
+//   const offsetHeight = card.offsetHeight;
+
+// if (top + windowHeight > offset && top < offset + offsetHeight ) {
+//   gsap.from('.advantages__card', {
+//     duration: 1.5,
+//       opacity: 0,
+//       x: -100,   
+//       stagger: {
+//         each: 0.5, 
+//         from: "start",
+//       }
+//     })
+  
+  
+// }
+
+
+
+// });
+
+// }
+
+
 
 
 const burgerIcon = document.querySelector('.burger-icon');
